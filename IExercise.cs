a0ce86009b070b518dfd446828276be404625100
ghoshsomik <somik.k.ghosh@gmail.com>
2014-11-08 17:56:13 -0500

    added interface for exercises

---
+++ IExercise.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EhT.Intrinsecus
+{
+    interface IExercise
+    {
+        bool Update(System.Windows.Media.DrawingContext ctx);
+
+    }
+}
