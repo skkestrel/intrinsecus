b6dbecea62b0b0ff9204f1e4c62bb2005a320d4a
sarahlynn <sarah.holdijk@gmail.com>
2014-11-08 22:30:40 -0800

    beginning for shoulder press

--- ShoulderPresses.cs
+++ ShoulderPresses.cs
@@ -10,9 +10,41 @@ namespace EhT.Intrinsecus
 {
     class ShoulderPresses : IExercise
     {
+        bool repComplete;
+        int reps;
+
+        public ShoulderPresses()
+		{
+			reps = 0;
+		}
+
         public int Update(Body body, DrawingContext ctx, Intrinsecus intrinsecus)
         {
-            return 0;
+            CameraSpacePoint leftShoulder = body.Joints[JointType.ShoulderLeft].Position;
+            CameraSpacePoint leftElbow = body.Joints[JointType.ElbowLeft].Position;
+            CameraSpacePoint leftWrist = body.Joints[JointType.WristLeft].Position;
+
+            CameraSpacePoint rightShoulder = body.Joints[JointType.ShoulderRight].Position;
+            CameraSpacePoint rightElbow = body.Joints[JointType.ElbowRight].Position;
+            CameraSpacePoint rightWrist = body.Joints[JointType.WristRight].Position;
+
+            double leftAngle = MathUtil.CosineLaw(leftWrist, leftShoulder, leftElbow);
+            double rightAngle = MathUtil.CosineLaw(rightWrist, rightShoulder, rightElbow);
+
+            if ((leftAngle > 120.0) && (rightAngle > 120.0))
+            {
+                if (!repComplete)
+                {
+                    reps++;
+                    repComplete = true;
+                }
+            }
+            else
+            {
+                repComplete = false;
+            }
+
+            return reps;
         }
 
         public string GetName()
