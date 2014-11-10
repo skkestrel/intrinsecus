e9abe92ddcbbe50f321a5f04d717fcf72250a56e
g2-aj <alshahna.jamal@gmail.com>
2014-11-10 00:45:31 -0800

    fixed squat!

--- Squat.cs
+++ Squat.cs
@@ -13,39 +13,82 @@ namespace EhT.Intrinsecus
 
 		private bool repComplete;
 
+        enum Transition
+        {
+            UPTODOWN,
+            DOWNTOUP,
+        }
+
+        private Transition state = Transition.UPTODOWN;
+
 		public Squat()
 		{
 			reps = 0;
 		}
 
 		public int Update(Body body, DrawingContext ctx, Intrinsecus intrinsecus)
-		{
-			float HipPoint = body.Joints[JointType.HipLeft].Position.Y;
-			float KneePoint = body.Joints[JointType.KneeLeft].Position.Y;
+        {
+		    
+            //left side
+			float HipPointL = body.Joints[JointType.HipLeft].Position.Y;
+			float KneePointL = body.Joints[JointType.KneeLeft].Position.Y;
 
-			float HipKnee = HipPoint - KneePoint;
+            //right side
+            float HipPointR = body.Joints[JointType.HipRight].Position.Y;
+            float KneePointR = body.Joints[JointType.KneeRight].Position.Y;
 
+			float HipKneeL = HipPointL - KneePointL;
+            float HipKneeR = HipPointR - KneePointR;
+
+            double HipToKneeAngle = MathUtil.CosineLaw(body.Joints[JointType.HipRight].Position, body.Joints[JointType.AnkleRight].Position, body.Joints[JointType.KneeRight].Position);
+
+            
             // y dimension logic up
-			if (HipKnee > 0.10)
+			if (HipKneeL > 0.20 && HipKneeR > 0.20)
 			{
-				if (repComplete)
-				{
-					reps++;
-					repComplete = false;
-				}
+                if (state == Transition.DOWNTOUP)
+                {
+                    reps++;
+                    state = Transition.UPTODOWN;
+                } 
+				
 			}
             // y dimension logic down
-			else if (HipKnee < 0.05)
+			else if (HipKneeL < 0.10 && HipKneeR < 0.10)
 			{
-				repComplete = true;
-                intrinsecus.InstructionLabel.Content = "Great squat";
-				repflashtick = 0;
+                if (state == Transition.UPTODOWN)
+                {
+                   
+                    if (HipToKneeAngle <= 90)
+                    {
+                  
+                        intrinsecus.InstructionLabel.Content = "Great squat";
+                        state = Transition.DOWNTOUP;
+                        repflashtick = 0;
+                    }
+                    else
+                    {
+                        intrinsecus.InstructionLabel.Content = "Squat form not low enough. Sit bro";
+                    }
+                }
 			}
-            else
-            {
-                intrinsecus.InstructionLabel.Content = "Squat form not low enough. Sit bro";
-                repComplete = false;
-            }
+          
+            
+
+           
+
+
+            //case: angle between femur and calf is greater than 180
+            //out: not low enough
+            //if flag was tripped, reset flag
+
+            // case: anglebetween femur and calf < 90 && > 75
+            //out rep done, go up now.
+            //if flag not tripped, add to rep
+
+            //case: anglebetween femur < 75
+            //warn too low
+            //  Debug.WriteLine("{0}", reps);
 
 			if (repflashtick++ <= 3)
 			{
@@ -53,6 +96,9 @@ namespace EhT.Intrinsecus
 
 				CameraSpacePoint position1 = body.Joints[JointType.HipLeft].Position;
 				CameraSpacePoint position2 = body.Joints[JointType.KneeLeft].Position;
+
+                CameraSpacePoint position3 = body.Joints[JointType.HipRight].Position;
+                CameraSpacePoint position4 = body.Joints[JointType.KneeRight].Position;
 
 				if (position1.Z < 0)
 				{
@@ -62,28 +108,27 @@ namespace EhT.Intrinsecus
 				{
 					position2.Z = 0.1f;
 				}
+                if (position3.Z < 0)
+                {
+                    position3.Z = 0.1f;
+                }
+                if (position4.Z < 0)
+                {
+                    position4.Z = 0.1f;
+                }
 
 				DepthSpacePoint depthSpacePoint1 = intrinsecus.CoordinateMapper.MapCameraPointToDepthSpace(position1);
 				DepthSpacePoint depthSpacePoint2 = intrinsecus.CoordinateMapper.MapCameraPointToDepthSpace(position2);
+                DepthSpacePoint depthSpacePoint3 = intrinsecus.CoordinateMapper.MapCameraPointToDepthSpace(position3);
+                DepthSpacePoint depthSpacePoint4 = intrinsecus.CoordinateMapper.MapCameraPointToDepthSpace(position4);
 
 				ctx.DrawLine(highlightPen, new Point(depthSpacePoint1.X, depthSpacePoint1.Y),
 					new Point(depthSpacePoint2.X, depthSpacePoint2.Y));
+                ctx.DrawLine(highlightPen, new Point(depthSpacePoint3.X, depthSpacePoint3.Y),
+                    new Point(depthSpacePoint4.X, depthSpacePoint4.Y));
 			}
 
-			//if(HipJoint.y - KneeJoint.y)
-
-
-			//case: angle between femur and calf is greater than 180
-			//out: not low enough
-			//if flag was tripped, reset flag
-
-			// case: anglebetween femur and calf < 90 && > 75
-			//out rep done, go up now.
-			//if flag not tripped, add to rep
-
-			//case: anglebetween femur < 75
-			//warn too low
-			//  Debug.WriteLine("{0}", reps);
+	
 			return reps;
 		}
 
